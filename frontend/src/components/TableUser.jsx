6fedc2b5ef43f1f9b4983104c6e7c100bd7bc1be
KhoaHo1207 <khoaho120703@gmail.com>
2024-05-31 10:06:33 +0700

    [Add]_Display List User_FE

---
+++ frontend/src/components/TableUser.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserService";
+
+function TableUser() {
+  const [listUser, setListUser] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+    if (res && res?.data && res?.data?.data && res.status === 200)
+      setListUser(res?.data?.data);
+  };
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUser.map((user, index) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.email}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+export default TableUser;
